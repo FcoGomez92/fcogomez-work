9a98c2e425a3db75c098f330d135fdcd0fade33c
Francisco Manuel Gómez Gallardo <francis.gg48@gmail.com>
2024-02-01 17:35:35 +0100

    add tinybird analytics

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 export function Analytics() {
-	const token = process.env.NEXT_PUBLIC_BEAM_TOKEN;
+	const token = process.env.NEXT_PUBLIC_TINYBIRD_TOKEN;
 	if (!token) {
 		return null;
 	}
 	return (
 		<script
-			src="https://beamanalytics.b-cdn.net/beam.min.js"
+			defer
+			src="https://unpkg.com/@tinybirdco/flock.js"
 			data-token={token}
-			async
 		/>
 	);
 }
